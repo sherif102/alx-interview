9898f0a63780a3e32b499822156b90c720854cdb
hyperbayse <hyperbayse@gmail.com>
2025-04-19 01:41:14 +0100

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,27 @@
-#!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""
+Pascal's triangle
+"""
 
 
-def pascal_triangle(n):
-    '''
-    Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
-    '''
+def pascal_triangle(n, return_list=[]):
+    """finds the pascal's value for the n number"""
+    def factorial(integer):
+        if integer <= 1:
+            return integer
+        return integer * factorial(integer - 1)
+
     if n <= 0:
         return []
-    elif n == 1:
-        return [[1]]
-    elif n == 2:
-        return [[1], [1, 1]]
-    triangle = [[1], [1, 1]]
-
-    for i in range(2, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+    elif n > 0:
+        pascal_triangle(n - 1, return_list)
+        result = [
+            factorial(n) // (factorial(n - base) * factorial(base))
+            for base in range(1, n)
+            ]
+        result.append(1)
+        if len(result) > 0:
+            result.insert(0, 1)
+        if n == 1:
+            return_list.append([1])
+        return_list.append(result)
+    return return_list
