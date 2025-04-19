c0baf7d4a07613f2a7c3ecba360a9da0ed602184
hyperbayse <hyperbayse@gmail.com>
2025-04-19 18:43:56 +0100

    pascal triangle correction

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,21 +8,18 @@ def pascal_triangle(n, return_list=[]):
     """finds the pascal's value for the n number"""
     def factorial(integer):
         if integer <= 1:
+            return 1
+        elif integer <= 1:
             return integer
         return integer * factorial(integer - 1)
 
-    if n <= 0:
+    if n < 0:
         return []
-    elif n > 0:
+    if n >= 0:
         pascal_triangle(n - 1, return_list)
-        result = [
+        row = [
             factorial(n) // (factorial(n - base) * factorial(base))
-            for base in range(1, n)
+            for base in range(n + 1)
             ]
-        result.append(1)
-        if len(result) > 0:
-            result.insert(0, 1)
-        if n == 1:
-            return_list.append([1])
-        return_list.append(result)
+        return_list.append(row)
     return return_list
