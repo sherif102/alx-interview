7c3c4974d72c4676dc6d60f9939067469b26f436
hyperbayse <hyperbayse@gmail.com>
2025-05-16 12:48:27 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# def canUnlockAll(boxes):
+#     boxes_checked = {tuple(boxes[0])}
+#     checker = [0]
+#     all_keys = {0}
+#     keys = set()
+
+#     while True:
+#         for key in checker:
+#             if boxes[key]:
+#                 boxes_checked.add(tuple(boxes[key]))
+#                 for new_key in boxes[key]:
+#                     keys.add(new_key)
+#         checker = list(keys)
+#         if keys.issubset(all_keys):
+#             if len(keys) == 0:
+#                 boxes_checked.add(list)
+#             break
+#         for copy_key in keys:
+#             all_keys.add(copy_key)
+#         keys.clear()
+#     return len(boxes) == len(boxes_checked)
+
+def canUnlockAll(boxes):
+    visited = set()
+    keys = [0]
+
+    while keys:
+        current = keys.pop()
+        if current not in visited and current < len(boxes):
+            visited.add(current)
+            for key in boxes[current]:
+                if key not in visited:
+                    keys.append(key)
+    return len(visited) == len(boxes)
