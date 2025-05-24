862ca2a2d03fed906575e4c2e4cb751d98f79a47
hyperbayse <hyperbayse@gmail.com>
2025-05-24 02:13:08 +0100

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""minimum operations"""
+
+
+def minOperations(n):
+    """calculates the minimum operations"""
+    if n == 1:
+        return 1
+    elif n < 1:
+        return 0
+
+    op_counter = 2
+    current = 2
+    copied = 1
+
+    while current < n:
+        if n % current == 0 and (n // current) % 2 == 0:
+            op_counter += 2
+            copied = current
+            current += copied
+        elif n % current == 0:
+            op_counter += 2
+            copied = current
+            current += copied
+        else:
+            op_counter += 1
+            current += copied
+
+    if current > n:
+        return 0
+
+    return op_counter
